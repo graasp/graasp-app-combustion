cfda9af247a9181a0de30042ea5d11fbd39327d2
Hagop Taminian <hagop.taminian@gmail.com>
2026-03-28 16:24:22 +0300

    feat: adjust display units in energy box
    closes #25

--- src/components/canvas/interaction-box/EnergyBox.tsx
+++ src/components/canvas/interaction-box/EnergyBox.tsx
@@ -1,23 +1,31 @@
+import { useContext } from 'react';
+
 import { BatteryChargingFull, Bolt } from '@mui/icons-material';
 
 import { PRIMARY_BLUE } from '@/constants/css';
+import { AppSettingsContext } from '@/contexts/AppSettingsProvider';
 
 import InteractionBox from './InteractionBox';
 
-const EnergyBox = (): JSX.Element => (
-  <InteractionBox
-    icon={<Bolt sx={{ color: PRIMARY_BLUE }} />}
-    mainText="ENERGY OBTAINED"
-    subText={
-      <>
-        ΔH<sub>comb</sub> [kJ/mol]
-      </>
-    }
-    value={3.46}
-    secondaryIcon={<BatteryChargingFull sx={{ color: '#006F0D' }} />}
-    secondaryText="200 1-week phone charges"
-    secondaryTextColor="#006F0D"
-  />
-);
+const EnergyBox = (): JSX.Element => {
+  const { state } = useContext(AppSettingsContext);
+  const { units } = state;
+
+  return (
+    <InteractionBox
+      icon={<Bolt sx={{ color: PRIMARY_BLUE }} />}
+      mainText="ENERGY OBTAINED"
+      subText={
+        <>
+          ΔH<sub>comb</sub> [kJ/{units}]
+        </>
+      }
+      value={3.46}
+      secondaryIcon={<BatteryChargingFull sx={{ color: '#006F0D' }} />}
+      secondaryText="200 1-week phone charges"
+      secondaryTextColor="#006F0D"
+    />
+  );
+};
 
 export default EnergyBox;
