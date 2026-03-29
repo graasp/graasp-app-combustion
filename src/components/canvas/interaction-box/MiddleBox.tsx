a68133a5edc26d147abb4b2f0dad5c488dbfe913
Hagop Taminian <hagop.taminian@gmail.com>
2026-03-29 14:28:44 +0300

    feat: add placeholder to energy/volume section
    closes #31

--- src/components/canvas/interaction-box/MiddleBox.tsx
+++ src/components/canvas/interaction-box/MiddleBox.tsx
@@ -14,12 +14,19 @@ interface Props {
   value: number;
 }
 
-const MiddleBox = ({ value }: Props): JSX.Element => (
-  <Box sx={styles}>
-    <Typography variant="h3" sx={{ fontWeight: 500 }}>
-      {formatNumber(value)}
-    </Typography>
-  </Box>
-);
+const MiddleBox = ({ value }: Props): JSX.Element => {
+  const isEmpty = value === 0;
+
+  return (
+    <Box sx={styles}>
+      <Typography
+        variant={isEmpty ? 'h6' : 'h4'}
+        sx={{ fontWeight: 500, opacity: isEmpty ? 0.5 : 1 }}
+      >
+        {isEmpty ? '- - -' : formatNumber(value)}
+      </Typography>
+    </Box>
+  );
+};
 
 export default MiddleBox;
